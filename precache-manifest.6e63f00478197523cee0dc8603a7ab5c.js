d3b4766c23f7b68e531e44b7fd89ed22a947a678
itsSayantan <sayantan.ghosh03@gmail.com>
2020-08-16 23:10:20 +0530

    Updates

---
+++ precache-manifest.6e63f00478197523cee0dc8603a7ab5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf1cd5ada48567a859542f4d7eb2160",
+    "url": "/wordpow/index.html"
+  },
+  {
+    "revision": "68f030aa3023986c65fe",
+    "url": "/wordpow/static/css/main.387514fc.chunk.css"
+  },
+  {
+    "revision": "f18189c73ab99b8ac6ae",
+    "url": "/wordpow/static/js/2.286ea655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wordpow/static/js/2.286ea655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f030aa3023986c65fe",
+    "url": "/wordpow/static/js/main.2b3fff83.chunk.js"
+  },
+  {
+    "revision": "4a5846e53804c271aa93",
+    "url": "/wordpow/static/js/runtime-main.de19762d.js"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/wordpow/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "169efc691ee2e350010e9c525a35a7d8",
+    "url": "/wordpow/static/media/pow.169efc69.svg"
+  }
+]);
